31a86f08a53d5169c61a01e1a24b991ad9cfa90c
Hernán Castañón Álvarez <hernanderes34@gmail.com>
2014-03-31 20:34:56 +0200

    Illusion Bring Up for Moto X

---
+++ ill.mk
@@ -0,0 +1,18 @@
+# Inherit some common Illusion stuff.
+$(call inherit-product, vendor/illusion/config/common_full_phone.mk)
+
+# Enhanced NFC
+$(call inherit-product, vendor/illusion/config/nfc_enhanced.mk)
+
+# Boot animation
+TARGET_SCREEN_WIDTH := 720
+TARGET_SCREEN_HEIGHT := 1280
+
+# Release name
+PRODUCT_NAME := ill_moto_msm8960dt
+
+$(call inherit-product, device/motorola/moto_msm8960dt/full_moto_msm8960dt.mk)
+
+# bootanimation
+PRODUCT_COPY_FILES += \
+        vendor/illusion/bootanimation/Illusion720x720.zip:system/media/bootanimation.zip
